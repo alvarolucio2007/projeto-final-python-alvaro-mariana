dcc6663b6b228a4c7cfb27440f33dfb27ebefc4b
Álvaro Lúcio <alvarolucio2007@proton.me>
2025-11-17 20:08:13 -0300

    começando classe...

--- teste.py
+++ teste.py
@@ -1,2 +1,10 @@
-while True:
-    print("Hello World!")
+#Sistema: Sistema de Livros (Título, Autor, Disponibilidade)
+#Funcionalidades Obrigatórias: CRUD (Create,Read,Update,Delete), Resumo/Relatório, Sair
+#   Funções com responsabilidades claras, tipo cadastrar_livro() apenas possuir cadastro, etc.
+#   Uso de Listas de Dicionários (Dica: ter ID único para cada cadastro,validações simples,testar cada função separadamente.
+import json
+
+import streamlit as st
+class BackEnd():
+    def __init__(self) -> None:
+        
