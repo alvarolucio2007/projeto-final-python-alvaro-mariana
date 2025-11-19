244600cca0b1f0d512fc635ff3914fcb588da5d4
Álvaro Lúcio <alvarolucio2007@proton.me>
2025-11-18 22:40:34 -0300

    Separação de Sistemas

---
+++ Codigo/app.py
@@ -0,0 +1,2 @@
+#Isso aqui vai ser só pra rodar tlgd?
+#Depois nós vamos configurar direito e tals
---
+++ Codigo/frontend.py
@@ -0,0 +1,4 @@
+#Esse é a part front-end do sistema, pra ficar mais organizado qualquer coisa avisa
+#Lembra de fazer o seu venv (o meu ta no gitignore, que não é visto pelo git e github), e baixar o streamlit (pip install streamlit, ja com venv ativado)
+import streamlit as st #Fazer o front-end em si, para rodar, vai no terminal e digita "streamlit run {caminho relativo} "
+st.title("Hello World!")
