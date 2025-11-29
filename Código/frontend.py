1787ab1863c3b23810440c796803d1f95394f963
Álvaro Lúcio Mousinho Coelho <alvarolucio2007@proton.me>
2025-11-29 18:42:35 -0300

    aaa

--- Código/frontend.py
+++ Código/frontend.py
@@ -6,12 +6,51 @@ class FrontEnd: #Recomendo fazer type hinting e POO pq fica chiquérrimo
     def __init__(self) -> None:
         self.estoque=BackEnd()
         self.produtos=self.estoque.lista_livros
+    def exibir_mensagens_pendentes(self): #Nos formulários, o streamlit gosta de resetar tudo com o st.rerun(), ent isso aq salva as mensagens de sucesso.
+        if 'success_message' in st.session_state:
+            st.success(st.session_state['success_message'])
+            del st.session_state['success_message']
     def renderizar_menu_lateral(self)->str:
         with st.sidebar:
             st.header("Selecione a ação")
-            opcao_selecionada:str=st.dropbox("Navegação",["Teste","teste","teste"])
+            opcao_selecionada:str=st.selectbox("Navegação",["Cadastrar Livro","Listar Livros","Buscar Livros","Atualizar Livros","Excluir Livros","Gerar Relatórios"])
             return opcao_selecionada
-    
-front=FrontEnd
-front.renderizar_menu_lateral
+    def renderizar_cadastro(self) -> None:
+        self.exibir_mensagens_pendentes()
+        st.markdown("Cadastro de novo livro")
+        with st.form("form_cadastro"):
+            titulo=st.text_input("Título do Livro",max_chars=100)
+            col1,col2=st.columns(2)
+            with col1:
+                autor=st.text_input("Autor do Livro",max_chars=100)
+            with col2:
+                ano=st.number_input("Ano de lançamento",min_value=0,format="%d",step=1)
+            col3,col4=st.columns(2)
+            with col3:
+                preco=st.number_input("Preço do Livro",min_value=0.01,format="%.2f",step=0.01)
+            with col4:
+                quantidade=st.number_input("Quantidade do Livro",min_value=0,format="%d",step=1)
+            clique_salvar=st.form_submit_button("Cadastrar e Salvar", width='stretch')
+            if clique_salvar:
+                try:
+                    self.estoque.cadastrar_livro(
+                        titulo=str(titulo),
+                        preco=float(preco),
+                        autor=str(autor),
+                        ano=int(ano),
+                        quantidade=int(quantidade)
+                    )
+                    
+                    st.session_state['success_message']=f"Produto '{titulo} adicionado com sucesso!"
+                    st.rerun()
+                except ValueError as e:
+                    st.error(f"Falha no cadastro! {e}")
+        return
+    def renderizar_atualizar(self)-> None:
+        pass
+                    
+if __name__=="__main__":
+    app=FrontEnd()
+    app.renderizar_cadastro() #Mude essa função aq pra mudar oq aparece na pagina do streamlit
+
     
